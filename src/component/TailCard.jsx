e38c87f5b7d9cb7739a6bdb98a9189b45c37639f
kimyuchan-1 <kimuchan1@gmail.com>
2025-10-28 13:51:06 +0900

    TailCard css

--- src/component/TailCard.jsx
+++ src/component/TailCard.jsx
@@ -1,14 +1,16 @@
 export default function TailCard({title, url, location, keyword}) {
-    
+    const keywordTags = keyword.map(item => {
+        return <p className="p-1 m-1 rounded-md bg-gray-200 text-gray-700">{item}</p>
+    });
     
     return (
-        <div className="flex flex-col">
-            <img src = {url} alt = {title}/>
-            <div>
-                <div>{title}</div>
-                <div>{location}</div>
-                <div>
-                    
+        <div className="flex flex-col w-auto">
+            <img src = {url} alt = {title} />
+            <div className="p-3">
+                <div className="font-bold text-gray-800">{title}</div>
+                <div className="text-sm text-gray-500 pt-2">{location}</div>
+                <div className="flex flex-wrap justify-start items-start mt-3">
+                    {keywordTags}
                 </div>
             </div>
         </div>
